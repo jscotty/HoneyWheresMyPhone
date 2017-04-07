22b6e8a6480fc8d4254a94d42171b052fe452e1a
Nick van Dokkum <nickvandokkum@hotmail.nl>
2017-04-07 11:27:25 +0200

    make a function to return if end object is grabbed

--- HoneyWheresMyPhone/Assets/Scripts/Collision/ItemScores.cs
+++ HoneyWheresMyPhone/Assets/Scripts/Collision/ItemScores.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class ItemScores : MonoBehaviour {
 
     [SerializeField] private int _score;
+    [SerializeField] private bool _endObject;
 
     public int Score() {
         return _score;
+    }
+
+    public bool AquiredEndObject() {
+        return _endObject;
     }
 }
