0a98178e1695b4bd1c96072f0e216943ee977134
Nick van Dokkum <nickvandokkum@hotmail.nl>
2017-04-20 14:49:07 +0200

    optimised the progressbar calculations

--- HoneyWheresMyPhone/Assets/Scripts/UI/ProgressBar.cs
+++ HoneyWheresMyPhone/Assets/Scripts/UI/ProgressBar.cs
@@ -13,64 +13,38 @@ public class ProgressBar : Singleton<ProgressBar> {
         set
         {
             _endItem = value;
-            _targetStartPos = value.position;
         }
     }
     private Transform _endItem;
+    private Vector2 _endItemPos;
     public Transform StartItem { private get; set; }
-        /* this is for debugging
-    //[SerializeField] private Transform _endItem;
-    //[SerializeField] private Transform _startItem;
+    /* //this is for debugging
+    //[SerializeField] private Transform _endItemSerializefield;
     */
-
-    private bool backToStart = false;
-
-    private Vector2 _targetStartPos;
-    
     [SerializeField] private RectTransform _uiObject;
 
     [SerializeField] private Transform _startPosTransform;
 
     private Vector2 _startPos;
-    [SerializeField] private Vector2 _uiObjectDesiredLocation;
-    private float _recordedPercentage;
+    private Vector2 _uiObjectDesiredLocation;
 
     private void Start()
     {
-        /* this is for debugging
-        //endItem = _endItem;
-        //startItem = _startItem;
+        /* //this is for debugging
+        //_endItem = _endItemSerializefield;
         */
-        //_targetStartPos = (Vector2)EndItem.position;
-        _startPos = -_uiObjectDesiredLocation;
+        _endItemPos = _endItem.position;
+        _startPos = _uiObject.anchoredPosition;
         StartItem = _startPosTransform;
+        _uiObjectDesiredLocation = -_uiObject.anchoredPosition;
     }
 
     private void Update()
     {
         float tPercentage = 0;
-        if(GameData.Instance.direction == Direction.DOWN)
-        {
-            tPercentage = EndItem.position.y / _targetStartPos.y;
-            Vector2 tPosition = Vector2.Lerp(_uiObjectDesiredLocation, _startPos,  1- tPercentage);
-            _uiObject.anchoredPosition = tPosition;
-            _recordedPercentage = tPercentage;
-        }
-        else if(GameData.Instance.direction == Direction.UP)
-        {
-            if (!backToStart)
-            {
-                _targetStartPos = StartItem.position;
-                _startPos = -_startPos;
-                backToStart = true;
-            }
-            tPercentage = StartItem.position.y / _targetStartPos.y;
-
-            Vector2 tPosition = Vector2.Lerp(-_uiObjectDesiredLocation, _startPos, 1-Mathf.Lerp(0,_recordedPercentage,tPercentage));
-            _uiObject.anchoredPosition = tPosition;
-        }
-#if UNITY_EDITOR
-        Debug.Log(tPercentage);
-#endif
+        tPercentage = StartItem.position.y / -_endItemPos.y;
+        Vector2 tPosition = Vector2.Lerp(_startPos, _uiObjectDesiredLocation, tPercentage);
+        //Debug.Log(tPercentage + " " + tPosition + " " + _startPos + " " + _uiObjectDesiredLocation);
+        _uiObject.anchoredPosition = tPosition;
     }
 }
